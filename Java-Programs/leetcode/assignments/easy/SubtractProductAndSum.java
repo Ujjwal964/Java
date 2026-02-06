1c6b17dc64e4c9a88aa639dc3eb9b84440ff6028
Ujjwal Bansal <ujjbansal964@gmail.com>
2026-02-06 18:51:02 +0530

    leetcode basics subtractProd&Sum

---
+++ Java-Programs/leetcode/assignments/easy/SubtractProductAndSum.java
@@ -0,0 +1,76 @@
+//QUES - https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+
+package leetcode.assignments.easy;
+import java.util.*;
+
+// -----------------------------------------PERSONAL( VSCODE )-----------------------------------------
+
+public class SubtractProductAndSum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter digit : ");
+        int n = sc.nextInt();
+
+        // -------------------EITHER THIS---------------------
+
+        int product = 1;
+        int sum = 0;
+
+        while(n>0){
+            int rem = n % 10;
+            product = rem * product;
+            sum = rem + sum;
+            n = n / 10;
+        }
+        System.out.println("Products of each digit is : "+product);
+        System.out.println("Sum of each digit is : "+sum);
+        System.out.println("Result(diff of product & sum) : " + (product-sum));
+
+
+        // -------------------OR---------------------
+
+        // int p = n;
+        // int s = n;
+
+        // int product = 1;
+        // int sum = 0;
+
+        // while(p>0){
+        //     int rem = p % 10;
+        //     product = rem * product;
+        //     p = p / 10;
+        // }
+        // System.out.println("Products of each digit is : "+product);
+
+        
+        // while(s>0){
+        //     int rem = s % 10;
+        //     sum = rem + sum;
+        //     s = s / 10;
+        // }
+        // System.out.println("Sum of each digit is : "+sum);
+
+        // System.out.println("Result(diff of product & sum) : " + (product-sum));
+    }
+}
+
+
+// ---------------------------------------------LEETCODE-----------------------------------------------
+
+// import java.util.*;
+
+// class Solution {
+//     public int subtractProductAndSum(int n) {
+//         int product = 1;                             //here we don't take input, it by default provided in func.
+//         int sum = 0;
+
+//         while(n>0){
+//             int rem = n % 10;
+//             product = rem * product;
+//             sum = rem + sum ;
+//             n = n / 10;
+//         }
+//         return (product - sum);              //avoid printing at console here and just direct return
+//     }
+// }
+
