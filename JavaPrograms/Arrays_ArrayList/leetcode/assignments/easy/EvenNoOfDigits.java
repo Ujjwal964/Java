4f741ff35c4fc1e9044affdb9ea3506e477bb449
Ujjwal Bansal <ujjbansal964@gmail.com>
2026-02-17 23:53:29 +0530

    leetcode assign easy even no digits

---
+++ JavaPrograms/Arrays_ArrayList/leetcode/assignments/easy/EvenNoOfDigits.java
@@ -0,0 +1,56 @@
+//QUES - https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+
+package Arrays_ArrayList.leetcode.assignments.easy;
+import java.util.*;
+
+// ----------------------------------------PERSONAL VSCODE-----------------------------------------
+
+public class EvenNoOfDigits {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int[] nums = new int[5];
+        System.out.print("Enter array : ");
+        for(int i=0;i<nums.length;i++){
+            nums[i] = sc.nextInt();
+        }
+        System.out.println("Even digit numbers are : "+countEvenDigits(nums));
+    }
+
+    static int countEvenDigits(int[] nums){
+        int even = 0;
+
+        for(int i=0;i<nums.length;i++){
+            int digit = 0;
+            int n = nums[i];
+            while(n>0){
+                n = n / 10;
+                digit++;
+            }
+
+            even += ( digit % 2 == 0 ) ? 1 : 0;
+        }
+        return even;
+    }
+}
+
+//12,345,2,6,7896
+
+// ----------------------------------------LEETCODE SOL-----------------------------------------
+// class Solution {
+//     public int findNumbers(int[] nums) {
+//         int even = 0;
+        
+//         for(int i=0;i<nums.length;i++){
+//             int n = nums[i];
+//             int digit = 0;
+//             while(n>0){
+//                 n = n / 10;
+//                 digit++;
+//             }
+
+//             even += (digit % 2 == 0) ? 1 : 0;
+//         }
+//         return even;
+//     }
+// }
--- JavaPrograms/Arrays_ArrayList/leetcode/assignments/easy/MatrixOddCellValues.java
+++ JavaPrograms/Arrays_ArrayList/leetcode/assignments/easy/MatrixOddCellValues.java
@@ -3,16 +3,66 @@
 package Arrays_ArrayList.leetcode.assignments.easy;
 import java.util.*;
 
-public class MatrixOddCellValues {
+// ----------------------------------------PERSONAL VSCODE-----------------------------------------
+
+public class MatrixOddCellValues{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
-        int[][] indices = {
-            {0,1},
-            {1,1}
-        };
-
-        // System.out.println("Odd values cells : " + oddValuesCells(2 , 3 , indices));
+        int[][] indices = { {1,1},{0,0} };
+        
+        System.out.println("Odd Cells are : "+oddCells(indices, 2, 3));
     }
     
+    static int oddCells(int[][] indices , int m , int n){
+        int[][] arr = new int[m][n];
+        int odd =0;
+        
+        for(int i=0;i<indices.length;i++){
+            int r = indices[i][0];
+            int c = indices[i][1];
+            
+            for(int j=0;j<n;j++){
+                arr[r][j]++;
+            }
+            for(int k=0;k<m;k++){
+                arr[k][c]++;
+            }
+        }
+        System.out.println("Final Matrix : "+Arrays.deepToString(arr));
+        
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                odd += ( arr[i][j] % 2 != 0 ) ? 1 : 0;
+            }
+        }
+        return odd;
+    }
 }
+
+
+// ----------------------------------------LEETCODE SOL-----------------------------------------
+// class Solution {
+//     public int oddCells(int m, int n, int[][] indices) {
+//         int[][] arr = new int[m][n];
+//         int odd = 0;
+
+//         for(int i=0;i<indices.length;i++){
+//             int r = indices[i][0];
+//             int c = indices[i][1];
+
+//             for(int j=0;j<n;j++){
+//                 arr[r][j]++;
+//             }
+//             for(int k=0;k<m;k++){
+//                 arr[k][c]++;
+//             }
+//         }
+
+//         for(int i=0;i<arr.length;i++){
+//             for(int j=0;j<arr[i].length;j++){
+//                 odd += (arr[i][j] % 2 != 0) ? 1 : 0;
+//             }
+//         }
+//         return odd;
+//     }
+// }
